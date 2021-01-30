89878cbe675983490c2f014682de14cff19c0c21
Yunyun <celeph4is@gmail.com>
2021-01-30 13:06:59 -0800

    add allBooks query: 8.2

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ const { ApolloServer, gql } = require('apollo-server')
 const books = require('./data/books.js').books;
 const authors = require('./data/authors.js').authors;
 
-/*
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
-*/
-
 // Schema type descriptions 
 // schema descriptions are kinda like typescript interfaces 
 // ! after value type means the value must be non-null 
@@ -28,12 +23,14 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
