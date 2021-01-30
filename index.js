4fa54bafc9f3f8849801380d8767e53a9c5693ea
Yunyun <celeph4is@gmail.com>
2021-01-30 13:12:17 -0800

    add allAuthors query;

--- index.js
+++ index.js
@@ -24,13 +24,15 @@ const typeDefs = gql`
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors
   }
 }
 
